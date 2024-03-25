0cc196038120ba3aaec5b2610900089cc2b37e8f
SergioSV25 <ssanchizv@gmail.com>
2024-03-25 12:55:29 +0100

    create superuser

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Empleado, Cliente, Tareas, Proyecto
 
-# Register your models here.
+# Registro de los modelos en la site de admin
+admin.site.register(Empleado, UserAdmin)
+admin.site.register(Cliente)
+admin.site.register(Tareas)
+admin.site.register(Proyecto)
