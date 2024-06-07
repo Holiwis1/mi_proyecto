62eccec255bb1fbb9e7abaa08f9d9f7d4dcfa956
Holiwis1 <123581831+Holiwis1@users.noreply.github.com>
2024-06-07 12:44:20 +0200

    css separado

--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -133,11 +133,14 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Configuración de la autentificacion
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 LOGIN_REDIRECT_URL = 'index'
 LOGIN_URL = '/iniciar-sesion/'
 LOGOUT_REDIRECT_URL = '/iniciar-sesion/'
 LOGOUT_URL = 'logout'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SESSION_COOKIE_SECURE = False
