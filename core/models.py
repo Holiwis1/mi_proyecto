217ed7b794fb73f9c5af092b76e6e73d0cd3670d
Holiwis1 <123581831+Holiwis1@users.noreply.github.com>
2024-06-19 10:39:12 +0200

    arreglado cliente

--- core/models.py
+++ core/models.py
@@ -60,7 +60,10 @@ class Cliente(models.Model):
     web = models.URLField(null=True, blank=True)
 
     def __str__(self):
-        return self.nombre + ' ' + self.nif
+        nombre = self.nombre or ''
+        nif = self.nif or ''
+        return f"{nombre} {nif}".strip()
+
         
 
 
